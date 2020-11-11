ba3758b4779580f0b5c75ece1f10a984f79b0473
RedHeadphone <huzaifa.k@somaiya.com>
2020-11-11 17:24:58 +0530

    ok

--- Frontend/src/store/index.js
+++ Frontend/src/store/index.js
@@ -3,7 +3,8 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
 export function configureStore() {
-    const store = createStore(rootReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+    const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
     return store;
 }
 
+//, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
