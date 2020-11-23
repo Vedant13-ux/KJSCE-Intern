088a18b8786fa0278a6d84b966fa0bd954fdf183
RedHeadphone <huzaifa.k@somaiya.com>
2020-11-23 17:20:48 +0530

    getting error in api

--- Backend/routes/community.js
+++ Backend/routes/community.js
@@ -4,6 +4,7 @@ const db = require('../models');
 
 // Getting Posts
 router.get('/posts/getAll', (req, res, next) => {
+    console.log("aya");
     db.Post.find().limit(10).exec()
         .then(posts => {
             res.status(200).send(posts);
