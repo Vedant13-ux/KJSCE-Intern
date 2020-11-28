b21139450427d75ecb925faa879c39829754539f
Vedant13-ux <56965523+Vedant13-ux@users.noreply.github.com>
2020-11-28 21:10:09 +0530

    redux

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 // axios.defaults.baseURL = "http://localhost:3001"
-axios.defaults.baseURL = "https://kjsce-connect-frontend.herokuapp.com";
+axios.defaults.baseURL = "http://localhost:3001";
 export function apiCall(method, path, data) {
     return new Promise((resolve, reject) => {
         return axios[method](path, data)
--- Frontend/src/store/index.js
+++ Frontend/src/store/index.js
@@ -3,8 +3,6 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 
 export function configureStore() {
-    const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
+    const store = createStore(rootReducer, compose(applyMiddleware(thunk)), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
     return store;
 }
-
-//, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
