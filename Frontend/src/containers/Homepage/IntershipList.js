105d9c7b9ebd3e179a0fb2cf5c2696b357971abe
Vedant13-ux <56965523+Vedant13-ux@users.noreply.github.com>
2020-12-01 00:10:12 +0530

    waste

--- Frontend/src/containers/Homepage/IntershipList.js
+++ Frontend/src/containers/Homepage/IntershipList.js
@@ -9,8 +9,8 @@ class InternshipList extends Component {
     super(props);
     this.state = {
       internships: [],
-      start:true,
-      query: ""
+      start: true,
+      searching: false
     };
   }
   componentWillMount() {
@@ -19,18 +19,18 @@ class InternshipList extends Component {
     let url = '/api/internship/search/all';
     apiCall('get', url, '')
       .then((internships) => {
-        return this.setState({ ...this.state,internships,start:false })
+        return this.setState({ ...this.state, internships, start: false })
       })
       .catch(err => {
         console.log(err);
-        return this.setState({ ...this.state,start:false})
-        
+        return this.setState({ ...this.state, start: false })
+
       })
   }
   // searchInternships(){}
 
   render() {
-    if (this.state.start){
+    if (this.state.start) {
       return (
         <div className="loading-anime">
           <Loading class="loading-wheel" />
@@ -38,37 +38,44 @@ class InternshipList extends Component {
       )
     }
     else {
-    return (
-      <div className="homeSection">
-        <div id="top-bar">
-          <button type="button" className="btn btn-default btn-circle btn-lg"><i class="fa fa-filter"></i>
-          </button>
-        </div>
-        <div id="intershiplist">
-          <div class="row">
-            {this.state.internships.map((internship) => {
-              return (
-                <Internship key={internship._id} {...internship}></Internship>
-              );
-            })}
+      return (
+        <div className="homeSection">
+          <div id="top-bar">
+            <button type="button" className="btn btn-default btn-circle btn-lg"><i class="fa fa-filter"></i>
+            </button>
           </div>
-          <MContext.Consumer>
-            {(context) => {
-              // if (this.state.query !== context.state.query && context.state.query !== "") {
-              //   return apiCall('get', '/api/internship/search/title/' + context.state.query, '')
-              //     .then((interns) => {
-              //       console.log(interns)
-              //     })
-              //     .catch(err => console.log(err))
-              // }
-              return <p>{context.state.query}</p>
-            }}
-          </MContext.Consumer>
+          <div id="intershiplist">
+            <div class="row">
+              {/* {this.state.searching === false &&
+                (
+                  this.state.internships.map((internship) => {
+                    return (
+                      <Internship key={internship._id} {...internship}></Internship>
+                    );
+                  })
+                )
+              } */}
+              {this.state.searching === false &&
+                (
+                  <MContext.Consumer>
+                    {(context) => {
+                      context.setInternships();
+                      context.state.internships.map((internship) => {
+                        return (
+                          <Internship key={internship._id} {...internship}></Internship>
+                        );
+                      })
+                    }}
+                  </MContext.Consumer>
+                )
+              }
+            </div>
+
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
-}
 }
 Internship.contextType = MContext;
 export default InternshipList;
--- Frontend/src/services/Provider.js
+++ Frontend/src/services/Provider.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
+import { apiCall } from './api';
 export const MContext = React.createContext();  //exporting context object
 export class MyProvider extends Component {
-    state = { query: "" }
+    state = { query: "", internships: [] }
     render() {
         return (
             <MContext.Provider value={
@@ -9,7 +10,17 @@ export class MyProvider extends Component {
                     state: this.state,
                     setMessage: (value) => this.setState({
                         query: value
-                    })
+                    }),
+                    setInternships: () => {
+                        return (
+                            apiCall('get', '/api/internship/search/title/' + this.state.query, '')
+                                .then((internships) => {
+                                    this.setState({ internships })
+                                }).catch((err) => {
+                                    console.log(err);
+                                })
+                        )
+                    }
                 }}>
                 {this.props.children}
             </MContext.Provider>)
