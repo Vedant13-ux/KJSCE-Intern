f052d4ce5c4a5d8565a5bb94e046774475971425
RedHeadphone <huzaifa.k@somaiya.com>
2020-11-30 15:58:24 +0530

    sticky done

--- Frontend/src/containers/Utils/EmailVerification.js
+++ Frontend/src/containers/Utils/EmailVerification.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom';
+// import { Redirect } from 'react-router-dom';
 import { apiCall } from '../../services/api';
 
 class Emailverification extends Component {
