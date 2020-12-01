ec70f787d8b247be9e77c921fd1d755569b92ccd
RedHeadphone <huzaifa.k@somaiya.com>
2020-12-01 18:46:27 +0530

    doing

--- Frontend/src/containers/Homepage/FilterForm.js
+++ Frontend/src/containers/Homepage/FilterForm.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { Multiselect } from "multiselect-react-dropdown";
 import { apiCall } from "../../services/api";
-import InputRange from 'react-input-range';
-import 'react-input-range/lib/css/index.css'
+import InputRange from "react-input-range";
+import "react-input-range/lib/css/index.css";
+import { MContext } from "../../services/Provider";
 
 class FilterForm extends Component {
   constructor(props) {
@@ -16,10 +17,10 @@ class FilterForm extends Component {
         { text: "C++" },
         { text: "React Native" },
       ],
-      value :{
+      value: {
         min: 3,
         max: 7,
-      }
+      },
     };
     this.handleSkills = this.handleSkills.bind(this);
     this.multiselectRef = React.createRef();
@@ -38,55 +39,60 @@ class FilterForm extends Component {
   }
   render() {
     return (
-      <div className="filterForm">
-        <label>By Skills</label>
-        <Multiselect
-          options={this.state.skills} // Options to display in the dropdown
-          selectedValues={this.state.skillsRequired} // Preselected value to persist in dropdown
-          // onSelect={this.onSelect} // Function will trigger on select event
-          // onRemove={this.onRemove} // Function will trigger on remove event
-          displayValue="text" // Property name to display in the dropdown options
-          onSearch={this.handleSkills}
-          ref={this.multiselectRef}
-        />
-        <div className="intType">
-          <label>Type</label>
-          <div
-            className="toggle-wrap"
-            title="Automatically append new posts in your feed"
-          >
-            <input id="autoUpdate" type="checkbox" defaultChecked></input>
-            <label htmlFor="autoUpdate">
-              <div className="toggle">
-                <div className="round"></div>
+      <MContext.Consumer>
+        {(context) => (
+          <div className="filterForm">
+            <label>By Skills</label>
+            <Multiselect
+              options={this.state.skills} // Options to display in the dropdown
+              selectedValues={this.state.skillsRequired} // Preselected value to persist in dropdown
+              // onSelect={this.onSelect} // Function will trigger on select event
+              // onRemove={this.onRemove} // Function will trigger on remove event
+              displayValue="text" // Property name to display in the dropdown options
+              onSearch={this.handleSkills}
+              ref={this.multiselectRef}
+            />
+            <div className="intType">
+              <label>Type</label>
+              <div
+                className="toggle-wrap"
+                title="Automatically append new posts in your feed"
+              >
+                <input id="autoUpdate" type="checkbox" defaultChecked></input>
+                <label htmlFor="autoUpdate">
+                  <div className="toggle">
+                    <div className="round"></div>
+                  </div>
+                  Work From Home
+                </label>
               </div>
-              Work From Home
-            </label>
-          </div>
-          <div
-            className="toggle-wrap"
-            title="Automatically append new posts in your feed"
-          >
-            <input id="autoUpdate" type="checkbox" defaultChecked></input>
-            <label htmlFor="autoUpdate">
-              <div className="toggle">
-                <div className="round"></div>
+              <div
+                className="toggle-wrap"
+                title="Automatically append new posts in your feed"
+              >
+                <input id="autoUpdate" type="checkbox" defaultChecked></input>
+                <label htmlFor="autoUpdate">
+                  <div className="toggle">
+                    <div className="round"></div>
+                  </div>
+                  External
+                </label>
               </div>
-              External
-            </label>
-          </div>
-        </div>
-        <InputRange
-        // formatLabel={value => `${value}months`}
-        maxValue={20}
-        minValue={0}
-        value={this.state.value}
-        onChange={value => this.setState({ ...this.state,  value })} />
+            </div>
+            <InputRange
+              // formatLabel={value => `${value}months`}
+              maxValue={20}
+              minValue={0}
+              value={this.state.value}
+              onChange={(value) => this.setState({ ...this.state, value })}
+            />
 
-        <button type="button" class="btn btn-default">
-          Apply Filters
-        </button>
-      </div>
+            <button type="button" class="btn btn-default">
+              Apply Filters
+            </button>
+          </div>
+        )}
+      </MContext.Consumer>
     );
   }
 }
