136e6a47c3f0341e16b72b22840189ce9bfed945
RedHeadphone <huzaifa.k@somaiya.com>
2020-12-12 21:08:46 +0530

    looks good

--- Frontend/src/compenents/Profile.js
+++ Frontend/src/compenents/Profile.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Navbar from "../containers/Global/Navbar";
 import PageFooter from "../containers/Global/PageFooter";
-// import { apiCall } from "../services/api";
+import { apiCall } from "../services/api";
 // import { Link } from 'react-router-dom';
 import Basic from '../containers/Profile/Basicinfo'
 import Moreinfo from '../containers/Profile/Moreinfo'
@@ -11,18 +11,52 @@ class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: {}
+      user: {
+        email: 'huzaifa@somaiya.edu',
+        // password: {
+        //   type: String,
+        //   required: true
+        // },
+        fname: 'huzaifa',
+        lname: 'khilawala',
+        dept: 'IT',
+        role: 'student',
+        year: '2nd',
+        rollNo: '1914095',
+        photo: 'https://www.jeumobi.com/wp-content/uploads/2020/10/among-us-mobile-240x240.png',
+        bookmarks: [],
+        notifications: [],
+        conversations: [],
+        interactions: [],
+        resume: 'ok',
+        internshipsOffered: [],
+        applications: [],
+        skills: ['python','java','bruh'],
+        posts: [],
+        liked: [],
+        commented: []
+      
+      
+      }
     }
+  }
+  componentDidMount(){
+    apiCall('get', '/api/user/profile/' + this.props.match.params.id, '').then(
+      (data)=>{
+        console.log(data);
+      }
+    )
   }
   render() {
     return (
       <div id="profile">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"></link>
         <Navbar currentUser={this.props.currentUser} />
         <section id="content" className="container">
-          <Basic/>
+          <Basic user={this.state.user}/>
           <div className="row">
-            <Moreinfo/>
-          <UserActivity/>
+            <Moreinfo user={this.state.user}/>
+          <UserActivity user={this.state.user}/>
           </div>
         </section>
         <PageFooter />
--- Frontend/src/containers/Profile/Moreinfo.js
+++ Frontend/src/containers/Profile/Moreinfo.js
@@ -1,54 +1,58 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-function Basic(props){
-    return (
-        <div className="col-md-4">
-              <div className="panel">
-                <div className="panel-heading">
-                  <span className="panel-icon">
-                    <i className="fa fa-star" />
-                  </span>
-                  <span className="panel-title"> User Popularity</span>
-                </div>
-                <div className="panel-body pn">
-                  <table className="table mbn tc-icon-1 tc-med-2 tc-bold-last">
-
-                  </table>
-                </div>
-              </div>
-              <div className="panel">
-                <div className="panel-heading">
-                  <span className="panel-icon">
-                    <i className="fa fa-trophy" />
-                  </span>
-                  <span className="panel-title"> My Skills</span>
-                </div>
-                <div className="panel-body pb5">
-                  <div className="tagsskill">ok</div>
-                  <div className="tagsskill">eating</div>
-                  <div className="tagsskill">sleeping</div>
-                  <div className="tagsskill">doing</div>
-                  <div className="tagsskill">ok</div>
-                  <div className="tagsskill">eating</div>
-                  <div className="tagsskill">sleeping</div>
-                  <div className="tagsskill">doing</div>
-                </div>
-              </div>
-              <div className="panel">
-                <div className="panel-heading">
-                  <span className="panel-icon">
-                    <i className="fa fa-pencil" />
-                  </span>
-                  <span className="panel-title">About Me</span>
-                </div>
-                <div className="panel-body pb5">
-
-                </div>
-              </div>
-            </div>
-    )
-
-
+function Basic(props) {
+  return (
+    <div className="col-md-4">
+      <div className="panel">
+        <div className="panel-heading">
+          <span className="panel-icon">
+            <i className="fa fa-star" />
+          </span>
+          <span className="panel-title">Info</span>
+        </div>
+        <div className="panel-body pn">
+          <table className="table mbn tc-icon-1 tc-med-2 tc-bold-last">
+            <tbody>
+              <tr>
+                <td>Department</td>
+                <td>{props.user.dept}</td>
+              </tr>
+              <tr>
+                <td>Year</td>
+                <td>{props.user.year}</td>
+              </tr>
+              <tr>
+                <td>Roll number</td>
+                <td>{props.user.rollNo}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="panel">
+        <div className="panel-heading">
+          <span className="panel-icon">
+            <i className="fa fa-trophy" />
+          </span>
+          <span className="panel-title">Skills</span>
+        </div>
+        <div className="panel-body pb5">
+          {props.user.skills.map((s, i) => {
+            return <div className="tagsskill">{s}</div>;
+          })}
+        </div>
+      </div>
+      <div className="panel">
+        <div className="panel-heading">
+          <span className="panel-icon">
+            <i className="fa fa-pencil" />
+          </span>
+          <span className="panel-title">Certificates</span>
+        </div>
+        <div className="panel-body pb5"></div>
+      </div>
+    </div>
+  );
 }
 
 export default Basic;
