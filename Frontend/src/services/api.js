02c239dac3b91d6aadb6f4845ce471484d00d9a1
Vedant13-ux <56965523+Vedant13-ux@users.noreply.github.com>
2020-12-17 18:07:20 +0530

    jwttoken

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,5 +1,13 @@
 import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:3001';
+
+export function setToken(token) {
+    if (token) {
+        axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    } else {
+        delete axios.defaults.headers.common["Authorization"];
+    }
+}
 // axios.defaults.baseURL = "https://kjsce-connect-backend.herokuapp.com";
 export function apiCall(method, path, data) {
     return new Promise((resolve, reject) => {
